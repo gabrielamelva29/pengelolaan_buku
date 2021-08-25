0e5645c7c456385f43ad85b14fb736b2ec3c71c6
Donytriprasetyo <donyprasetyo84@gmail.com>
2021-08-25 10:34:13 +0700

    Dony
    Percobaan 1

--- PengelolaanBuku/src/pengelolaanbuku/PengelolaanBuku.java
+++ PengelolaanBuku/src/pengelolaanbuku/PengelolaanBuku.java
@@ -15,7 +15,7 @@ public class PengelolaanBuku {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Pengelolaan Buku");
     }
     
 }
